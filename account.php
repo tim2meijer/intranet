2d7276d5ede9ea592b6ed7511684bbfff7c9bdf0
Matthijs Draijer <git@draijer.org>
2016-06-30 16:13:36 +0200

    Verduidelijking bij account-gegevens

--- account.php
+++ account.php
@@ -6,13 +6,6 @@ $cfgProgDir = 'auth/';
 include($cfgProgDir. "secure.php");
 $db = connect_db();
 
-/*
-if(!isset($_REQUEST['id'])) {
-	$id = $_SESSION['ID'];
-} else {
-	$id = $_REQUEST['id'];
-}
-*/
 $id = getParam('id', $_SESSION['ID']);
 
 # Als je niet voorkomt in de Admin-groep dan ga je naar je eigen gegevens
@@ -40,45 +33,48 @@ if(isset($_POST['data_opslaan']) AND $unique) {
 		$text[] = "Account succesvol gewijzigd.";
 		toLog('info', $_SESSION['ID'], $_POST['id'], 'Accountgegevens gewijzigd');
 	}			
-} else {		
+} else {
 	$text[] = "<form action='". $_SERVER['PHP_SELF'] ."' method='post'>";
 	$text[] = "<input type='hidden' name='id' value='$id'>";
 	$text[] = "<table border=0 width=100%>";
 	$text[] = "<tr>";
-	$text[] = "<td width=4%>&nbsp;</td>";
-	$text[] = "<td width=44%><h1>Accountgegevens</h1></td>";
-	$text[] = "<td width=4%>&nbsp;</td>";
-	$text[] = "<td width=44%>&nbsp;</td>";
-	$text[] = "<td width=4%>&nbsp;</td>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
+	$text[] = "	<td width=44%><h1>Accountgegevens</h1></td>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
+	$text[] = "	<td width=44%>&nbsp;</td>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
 	$text[] = "</tr>";
 	$text[] = "<tr>";
-	$text[] = "<td width=4%>&nbsp;</td>";
-	$text[] = "<td width=44% valign='top'>";
-	$text[] = "<table width='100%' border=0>";
-	$text[] = "<tr>";
-	$text[] = "	<td valign='top'>Gebruikersnaam</td>";
-	$text[] = "	<td valign='top'><input type='text' name='username' value='".$personData['username']."'></td>";
-	$text[] = "</tr>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
+	$text[] = "	<td width=44% valign='top'>";
+	$text[] = "	<table width='100%' border=0>";
+	$text[] = "	<tr>";
+	$text[] = "		<td colspan='2'>Mocht u niet tevreden zijn met uw huidige gebruikersnaam en wachtwoord dan kunt u die hier wijzigen.</td>";
+	$text[] = "	</tr>";
+	$text[] = "	<tr>";
+	$text[] = "		<td valign='top'>Gebruikersnaam</td>";
+	$text[] = "		<td valign='top'><input type='text' name='username' value='".$personData['username']."'></td>";
+	$text[] = "	</tr>";
 	
 	if($melding != '') {
-		$text[] = "<tr>";
-		$text[] = "	<td valign='top'>&nbsp;</td>";
-		$text[] = "	<td valign='top'>$melding</td>";
-		$text[] = "</tr>";
+		$text[] = "	<tr>";
+		$text[] = "		<td valign='top'>&nbsp;</td>";
+		$text[] = "		<td valign='top'>$melding</td>";
+		$text[] = "	</tr>";
 	}
 		
-	$text[] = "<tr>";
-	$text[] = "	<td valign='top'>Wachtwoord</td>";
-	$text[] = "	<td valign='top'><input type='text' name='wachtwoord' value=''></td>";
-	$text[] = "</tr>";
-	$text[] = "<tr>";
-	$text[] = "	<td colspan='5' align='center'><input type='submit' name='data_opslaan' value='Opslaan'></td>";
-	$text[] = "</tr>";
-	$text[] = "</table>";
-	$text[] = "</td>";
-	$text[] = "<td width=4%>&nbsp;</td>";
-	$text[] = "<td width=44%>&nbsp;</td>";
-	$text[] = "<td width=4%>&nbsp;</td>";
+	$text[] = "	<tr>";
+	$text[] = "		<td valign='top'>Wachtwoord</td>";
+	$text[] = "		<td valign='top'><input type='text' name='wachtwoord' value=''></td>";
+	$text[] = "	</tr>";
+	$text[] = "	<tr>";
+	$text[] = "		<td colspan='5' align='center'><input type='submit' name='data_opslaan' value='Opslaan'></td>";
+	$text[] = "	</tr>";
+	$text[] = "	</table>";
+	$text[] = "	</td>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
+	$text[] = "	<td width=44%>&nbsp;</td>";
+	$text[] = "	<td width=4%>&nbsp;</td>";
 	$text[] = "</tr>";
 	$text[] = "</table>";
 }
