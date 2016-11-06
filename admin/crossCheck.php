f6a39d4307523099f96d4d9996ae7d07c3088cf0
Matthijs Draijer <git@draijer.org>
2016-11-06 21:57:06 +0100

    varia

--- admin/crossCheck.php
+++ admin/crossCheck.php
@@ -7,16 +7,50 @@ $cfgProgDir = '../auth/';
 include($cfgProgDir. "secure.php");
 $db = connect_db();
 
-$sql = "SELECT * FROM $TablePlanning GROUP BY $PlanningUser";
+$sql = "SELECT $TablePlanning.$PlanningUser FROM $TablePlanning, $TableUsers, $TableAdres WHERE  $TablePlanning.$PlanningUser = $TableUsers.$UserID AND $TableUsers.$UserAdres = $TableAdres.$AdresID AND $TableAdres.$AdresMail like '' AND $TableUsers.$UserMail like '' GROUP BY $TablePlanning.$PlanningUser";
 $result = mysqli_query($db, $sql);
 if($row = mysqli_fetch_array($result)) {
 	do {
-		$data = getMemberDetails($row[$PlanningUser]);
-		
-		if($data['mail'] == '') {
-			echo makeName($row[$PlanningUser], 5) .' is niet in het bezit van een mailadres<br>';
-		}		
+		echo makeName($row[$PlanningUser], 5) .' ['. $row[$PlanningUser] .'] is niet in het bezit van een mailadres<br>';
 	} while($row = mysqli_fetch_array($result));		
 }
+
+/*
+$sql = "SELECT * FROM $TablePlanning, $TableGrpUsr, $TableDiensten WHERE $TablePlanning.$PlanningGroup = $TableGrpUsr.$GrpUsrGroup AND $TablePlanning.$PlanningUser = $TableGrpUsr.$GrpUsrUser AND $TablePlanning.$PlanningDienst = $TableDiensten.$DienstID AND $TableDiensten.$DienstStart > ". time();
+
+echo $sql;
+
+
+$result = mysqli_query($db, $sql);
+if($row = mysqli_fetch_array($result)) {
+	do {
+		echo makeName($row[$PlanningUser], 5) .' ['. $row[$PlanningUser] .'] is niet in het bezit van een mailadres<br>';
+	} while($row = mysqli_fetch_array($result));		
+}
+*/
+
+$roosters = getRoosters();
+
+foreach($roosters as $rooster) {
+	$details = getRoosterDetails($rooster);
+	$leden = getGroupMembers($details['groep']);
+	
+	$sql = "SELECT $TablePlanning.$PlanningUser FROM $TablePlanning, $TableDiensten WHERE $TablePlanning.$PlanningGroup = ". $details['groep'] ." AND $TablePlanning.$PlanningDienst = $TableDiensten.$DienstID  AND $TableDiensten.$DienstStart > ". time();
+		
+	$result = mysqli_query($db, $sql);
+	if($row = mysqli_fetch_array($result)) {
+		do {
+			makeName($row[$PlanningUser], 5) .' gevonden<br>';
+			
+			$key = array_search($row[$PlanningUser], $leden);
+			unset($leden[$key]);
+		} while($row = mysqli_fetch_array($result));		
+	}	
+	
+	foreach($leden as $lid) {
+		echo makeName($lid, 5) .' ['. $lid .'] staat niet op het rooster voor '. $details['naam'] .'<br>';
+	}	
+}
+
 
 ?>
---
+++ admin/export2Google.php
@@ -0,0 +1,84 @@
+<?php
+include_once('../include/functions.php');
+include_once('../include/config.php');
+include_once('../include/HTML_TopBottom.php');
+$requiredUserGroups = array(1);
+$cfgProgDir = '../auth/';
+include($cfgProgDir. "secure.php");
+$db = connect_db();
+
+if(!isset($_REQUEST['groep'])) {
+	echo "geen groep gedefinieerd";
+	exit;
+}
+
+$leden = getGroupMembers($_REQUEST['groep']);
+$groupData = getGroupDetails($_REQUEST['groep']);
+
+$kop[] = 'Given Name';
+$kop[] = 'Family Name';
+$kop[] = 'Additional Name';
+$kop[] = 'Initials';
+$kop[] = 'Maiden Name';
+$kop[] = 'Birthday';
+$kop[] = 'Gender';
+$kop[] = 'Group Membership';
+$kop[] = 'E-mail 1 - Value';
+$kop[] = 'E-mail 2 - Value';
+$kop[] = 'Phone 1 - Value';
+$kop[] = 'Phone 2 - Value';
+$kop[] = 'Address 1 - Type';
+$kop[] = 'Address 1 - Street';
+$kop[] = 'Address 1 - City';
+$kop[] = 'Address 1 - Postal Code';
+
+$output  = implode(";", $kop)."\n";
+
+foreach($leden as $lid) {
+	$data = getMemberDetails($lid);
+	
+	$veld = array();
+	$veld[] = $data['voornaam'];
+	$veld[] = $data['achternaam'];
+	$veld[] = $data['tussenvoegsel'];
+	$veld[] = $data['voorletters'];
+	$veld[] = $data['meisjesnaam'];
+	$veld[] = $data['geboorte'];
+	$veld[] = $data['geslacht'];
+	$veld[] = $groupData['naam'];
+	
+	if($data['prive_mail'] == '') {
+		$veld[] = $data['fam_mail'];
+		$veld[] = '';
+	} else {
+		$veld[] = $data['prive_mail'];
+		$veld[] = $data['fam_mail'];
+	}
+	
+	if($data['prive_tel'] == '') {
+		$veld[] = $data['fam_tel'];
+		$veld[] = '';
+	} else {
+		$veld[] = $data['prive_tel'];
+		$veld[] = $data['fam_tel'];
+	}
+	
+	$veld[] = 'Home';
+	$veld[] = $data['straat']	.' '.	$data['huisnummer'];
+	$veld[] = $data['plaats'];
+	$veld[] = $data['PC'];
+	
+	$output .= implode(";", $veld)."\n";
+}
+
+$file_name = $groupData['naam'].'.csv';
+
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="'.$file_name.'"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length:'.strlen($output));
+echo $output;
+exit;
