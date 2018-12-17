e40be1883abd1f8bd215b843074e79c2261d7619
Matthijs Draijer <git@draijer.org>
2018-12-17 16:45:16 +0100

    Update mailVoorganger.php

--- mailVoorganger.php
+++ mailVoorganger.php
@@ -42,42 +42,14 @@ foreach($diensten as $dienst) {
 		$aanspeekNaam = lcfirst($voorgangerData['titel']).' '.$voorgangerAchterNaam;
 		$mailNaam = $voorgangerData['init'].' '.$voorgangerAchterNaam;
 	}
-				
-	$mail = array(); 
-	$mail[] = "Beste $aanspeekNaam,";
-	$mail[] = "";
-	$mail[] = "Fijn dat u komt preken in de $dagdeel van ". strftime ('%e %B', $dienstData['start'])." om ". date('H:i', $dienstData['start'])." uur, in de Koningskerk te Deventer.";
-	$mail[] = "Ik geef u de nodige informatie door.";
-	$mail[] = "";
-	$mail[] = "De band wordt verzorgd door ". makeName($bandleider, 5) .".";
-	$mail[] = "Schriftlezing door ". makeName($schriftlezer, 5) .".";
-	$mail[] = "";
-	$mail[] = "U kunt de liturgie afstemmen met ". makeName($bandleider, 1) ." voor de muziek. ". ($bandData['geslacht'] == 'M' ? 'Hij' : 'Zij') ." kan dan aangeven of liederen bekend en of geschikt zijn in onze gemeente en eventuele suggesties voor een vervangend lied.";
-	$mail[] = "Wilt u de liturgie een week van te voren doorgeven zodat de band kan oefenen.";
-	$mail[] = "Als u deze mail beantwoordt met \"allen\" dan is iedereen op tijd op de hoogte.";
-	$mail[] = "";
-	$mail[] = "In de bijlage treft u de aandachtspunten van de dienst en het declaratieformulier aan.";
-	$mail[] = "";
-	$mail[] = "Mochten er nog vragen zijn dan hoor ik het graag.";
-	$mail[] = "";
-	$mail[] = "Vriendelijke groeten";
-	$mail[] = "";
-	$mail[] = "Jenny van der Vegt-Huzen";
-	$mail[] = "Tel.: 06-10638291";
-	$mail[] = "jenny@overbrugger.nl";
 	
-	$Subject = "Preken $dagdeel ". date('j-n-Y', $dienstData['start']);
-	
-	$HTMLMail = $MailHeader.implode("<br>\n", $mail).$MailFooter;	
-	$html =& new html2text($HTMLMail);
-	$html->set_base_url($ScriptURL);
-	$PlainMail = $html->get_text();
-	
+	# Nieuw mail-object aanmaken
 	$mail = new PHPMailer;
 	$mail->FromName	= "Jenny van der Vegt-Huzen";
-	$mail->From			= "jenny@overbrugger.nl";
+	$mail->From	= "jenny@overbrugger.nl";
 	
 	/*
+	# Alle geadresseerden toevoegen
 	$mail->AddAddress($voorgangerData['mail'], $mailNaam);
 	$mail->AddCC($bandData['mail'], makeName($bandleider, 5));
 	$mail->AddCC($schriftData['mail'], makeName($schriftlezer, 5));
@@ -86,21 +58,58 @@ foreach($diensten as $dienst) {
 	$mail->AddBCC("Jenny van der Vegt-Huzen", "jenny@overbrugger.nl");
 	$mail->AddBCC('internet@draijer.org');
 	*/
-		
-	$mail->AddAttachment('download/aandachtspunten.pdf', 'Aandachtspunten Liturgie Deventer (dd 11-6-2018).pdf');
-	$mail->AddAttachment('download/declaratieformulier.xlsx', date('ymd', $dienstData['start'])."_declaratieformulier_". str_replace(' ', '', $voorgangerAchterNaam) .".xlsx");
-	
 	$mail->AddAddress('matthijs@draijer.org', $mailNaam);
+	
+	# Mail opstellen
+	$mailText = array(); 
+	$mailText[] = "Beste $aanspeekNaam,";
+	$mailText[] = "";
+	$mailText[] = "Fijn dat u komt preken in de $dagdeel van ". strftime ('%e %B', $dienstData['start'])." om ". date('H:i', $dienstData['start'])." uur, in de Koningskerk te Deventer.";
+	$mailText[] = "Ik geef u de nodige informatie door.";
+	$mailText[] = "";
+	$mailText[] = "De band wordt verzorgd door ". makeName($bandleider, 5) .".";
+	$mailText[] = "Schriftlezing door ". makeName($schriftlezer, 5) .".";
+	$mailText[] = "";
+	$mailText[] = "U kunt de liturgie afstemmen met ". makeName($bandleider, 1) ." voor de muziek. ". ($bandData['geslacht'] == 'M' ? 'Hij' : 'Zij') ." kan dan aangeven of liederen bekend en of geschikt zijn in onze gemeente en eventuele suggesties voor een vervangend lied.";
+	$mailText[] = "Wilt u de liturgie een week van te voren doorgeven zodat de band kan oefenen.";
+	$mailText[] = "Als u deze mail beantwoordt met \"allen\" dan is iedereen op tijd op de hoogte.";
+	
+	# Bij gast-predikanten (niet zijnde Evert en Wim) moeten er bijlages bij
+	if($dienstData['voorganger_id'] != 15 AND $dienstData['voorganger_id'] != 52) {
+		$mailText[] = "";
+		$mailText[] = "In de bijlage treft u de aandachtspunten van de dienst en het declaratieformulier aan.";
+				
+		$mail->AddAttachment('download/aandachtspunten.pdf', 'Aandachtspunten Liturgie Deventer (dd 11-6-2018).pdf');
+		$mail->AddAttachment('download/declaratieformulier.xlsx', date('ymd', $dienstData['start'])."_declaratieformulier_". str_replace(' ', '', $voorgangerAchterNaam) .".xlsx");	
+	}
+	
+	$mailText[] = "";
+	$mailText[] = "Mochten er nog vragen zijn dan hoor ik het graag.";
+	$mailText[] = "";
+	$mailText[] = "Vriendelijke groeten";
+	$mailText[] = "";
+	$mailText[] = "Jenny van der Vegt-Huzen";
+	$mailText[] = "Tel.: 06-10638291";
+	$mailText[] = "jenny@overbrugger.nl";
+	
+	# Onderwerp maken
+	$Subject = "Preken $dagdeel ". date('j-n-Y', $dienstData['start']);
+	
+	# HTML- en plaintext-mail maken en deze toekennen aan het mail-object
+	$HTMLMail = $MailHeader.implode("<br>\n", $mailText).$MailFooter;	
+	$html =& new html2text($HTMLMail);
+	$html->set_base_url($ScriptURL);
+	$PlainMail = $html->get_text();
 	
 	$mail->Subject	= trim($Subject);
 	$mail->IsHTML(true);
-	$mail->Body			= $HTMLMail;
+	$mail->Body	= $HTMLMail;
 	$mail->AltBody	= $PlainMail;
 	
 	if(!$mail->Send()) {
-		toLog('error', '', '', 'Problemen met voorgangersmail versturen');
+		toLog('error', '', '', "Problemen met voorgangersmail versturen naar $mailNaam voor dienst $dienst.");
 	} else {
-		toLog('info', '', '', 'Voorgangersmail verstuurd');
+		toLog('info', '', '', "Voorgangersmail verstuurd naar $mailNaam voor dienst $dienst.");
 	}
 	
 	/*
