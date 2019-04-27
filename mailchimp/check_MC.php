81a6d3dcf206597d78ec90b3ebb6c6346820e890
Matthijs Draijer <git@draijer.org>
2019-04-27 19:26:30 +0200

    Update check_MC.php
    * Check of adres maar 1x voorkomt in lokale database
    * typo verbeterd

--- mailchimp/check_MC.php
+++ mailchimp/check_MC.php
@@ -22,7 +22,7 @@ if(count($data) > 0) {
 		$sql = "SELECT * FROM $TableMC WHERE $MCmail like '$email'";
 		$result = mysqli_query($db, $sql);
 				
-		if(mysqli_num_rows($result) > 0) {
+		if(mysqli_num_rows($result) == 1) {
 			$row = mysqli_fetch_array($result);
 			$wijk	=	$row[$MCwijk];			
 			$segment_id = $tagWijk[$wijk];
@@ -38,9 +38,11 @@ if(count($data) > 0) {
 			if($row[$MCstatus] != $rij['status'] AND $row[$MCstatus] != 'blocked')	toLog('error', '', $row[$MCID], "Volgens MailChimp is $email ". $rij['status'] .", volgende de lokale database niet");			
 			if($row[$MCfname] != $rij['voornaam'])																	toLog('error', '', $row[$MCID], "Volgens Mailchimp is de voornaam van $email ". $rij['voornaam'] .", volgens de lokale database ". $row[$MCfname]);
 			if(urldecode($row[$MCtname]) != $rij['tussen'])													toLog('error', '', $row[$MCID], "Volgens Mailchimp is het tussenvoegsel van $email .". $rij['tussen'] .", volgens de lokale database ". $row[$MCtname]);
-			if($row[$MClname] != $rij['achter'])																		toLog('error', '', $row[$MCID], "Volgens Mailchimp is de voornaam van $email ". $rij['achternaam'] .", volgens de lokale database ". $row[$MClname]);
+			if($row[$MClname] != $rij['achter'])																		toLog('error', '', $row[$MCID], "Volgens Mailchimp is de achternaam van $email ". $rij['achternaam'] .", volgens de lokale database ". $row[$MClname]);
 			if(!array_key_exists($tagScipio, $rij['tags']))													toLog('error', '', $row[$MCID], "Scipio-tag ontbreekt in MailChimp ($email staat wel in lokale database)");
 			if(!array_key_exists($segment_id, $rij['tags']) AND $wijk != '')				toLog('error', '', $row[$MCID], "Wijk-tag (wijk $wijk) ontbreekt in MailChimp ($email staat wel in lokale database)");
+		} elseif(mysqli_num_rows($result) > 1) {
+			toLog('error', '', $rij['scipio'], "$email komt meer dan 1x voor in de lokale database");
 		} elseif(array_key_exists($tagScipio, $rij['tags']) AND $rij['status'] != 'unsubscribed') {
 			toLog('error', '', $rij['scipio'], $rij['scipio'] ." komt wel voor in MailChimp, maar niet in lokale database");
 		} else {
